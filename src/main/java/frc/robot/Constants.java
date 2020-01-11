8731ed7a3acf1760d98a06b752f113a2d56b166f
michael <michael.adragna1@gmail.com>
2020-01-10 20:37:28 -0500

    Initial Commit. Created chassis subsystem and started the AutoDriveSequence Command.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import java.util.ArrayList;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final class MotorConstants{
+        public static final int kLeftMotorPort = 0;
+        public static final int kRightMotorPort = 1;
+    }
+
+    public static final class EncoderConstants{
+        public static final int kRightEncoderA = 0;
+        public static final int kRightEncoderB = 1;
+        public static final int kLeftEncoderA = 2;
+        public static final int kLeftEncoderB = 3;
+    }
+    
+  
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/ChassisSubsystem.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ChassisSubsystem extends SubsystemBase {
+
+  private final SpeedController rightMotor;
+  private final SpeedController leftMotor;
+  private final SpeedControllerGroup motors;
+  
+  private final Encoder rightEncoder;
+  private final Encoder leftEncoder;
+
+  private final DifferentialDrive driveBase;
+
+  /**
+   * Creates a new ExampleSubsystem.
+   */ 
+
+  public ChassisSubsystem() {
+    leftMotor = new Spark(Constants.MotorConstants.kLeftMotorPort);
+    rightMotor = new Spark(Constants.MotorConstants.kRightMotorPort);  
+    rightMotor.setInverted(true);
+    motors = new SpeedControllerGroup(rightMotor, leftMotor);
+
+    driveBase = new DifferentialDrive(leftMotor, rightMotor);
+    driveBase.setRightSideInverted(false);
+
+    
+    leftEncoder = new Encoder(Constants.EncoderConstants.kLeftEncoderA, Constants.EncoderConstants.kLeftEncoderB, true);
+    rightEncoder = new Encoder(Constants.EncoderConstants.kRightEncoderA, Constants.EncoderConstants.kRightEncoderB, false);
+  }
+
+  public void drive(double speed, double angle) {
+    driveBase.arcadeDrive(speed, angle);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
