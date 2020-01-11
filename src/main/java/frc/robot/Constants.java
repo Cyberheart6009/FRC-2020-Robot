b9da0ee39b0816a1d6c96de131ce6bf5c3a27d38
Stephen Clark <stephenthepro25@gmail.com>
2020-01-11 13:25:37 -0500

    get distance, reset encoders, get value within chassis subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,10 +35,12 @@ public final class Constants {
         public static final int kRightEncoderB = 1;
         public static final int kLeftEncoderA = 2;
         public static final int kLeftEncoderB = 3;
+        //placeholder value
+        public static final double ENCODER_COUNTS_PER_INCH = 13.49;
     }
 
     public static final class CameraConstants{
-        // 69 is a placeholder number since we don't know what port it will be plugged into yet.
+        // placeholder values since we don't know what servo they will be plugged into yet
         public static final int kCameraXServoRange = 69;
         public static final int kCameraYServoRange = 420;
 
