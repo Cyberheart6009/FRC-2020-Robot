76e9722e280c6d3e374e3356095f835609445554
Stephen Clark <stephenthepro25@gmail.com>
2020-01-11 12:43:40 -0500

    Created the Joystick Command, and fixed joystick controls so that the camera servos move according to joystick manipulation.

---
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.Constants;
+
+
+public class CameraSubsystem extends SubsystemBase {
+  private final Servo cameraX;
+  private final Servo cameraY;
+  
+
+
+  public CameraSubsystem() {
+      cameraX = new Servo(Constants.CameraConstants.kCameraXServoRange);
+      cameraY = new Servo(Constants.CameraConstants.kCameraYServoRange);
+  }
+
+  public void SetServos(double x, double y) {
+    cameraX.setAngle(x);
+    cameraY.setAngle(y);
+  }
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
