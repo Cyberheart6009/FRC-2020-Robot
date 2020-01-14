44e97450516c202058d204589c7c6b87864adadc
Daniel Hocevar <danielhocevar1@gmail.com>
2020-01-13 19:55:44 -0500

    ball tracker filters working

--- raspberrypi/pi stuff/this_year/ball_tracker.py
+++ raspberrypi/pi stuff/this_year/ball_tracker.py
@@ -26,7 +26,7 @@ class GripPipeline:
 
         self.__find_blobs_input = self.hsv_threshold_output
         self.__find_blobs_min_area = 0.0
-        self.__find_blobs_circularity = [0.51654675658658255, 1.0]
+        self.__find_blobs_circularity = [0.31654675658658255, 1.0]
         self.__find_blobs_dark_blobs = False
 
         self.find_blobs_output = None
@@ -118,7 +118,7 @@ BlurType = Enum('BlurType', 'Box_Blur Gaussian_Blur Median_Filter Bilateral_Filt
 
 pipeline = GripPipeline()
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
 #cap.set(15, -11)
@@ -200,13 +200,13 @@ while True:
     #frame2 = cv2.GaussianBlur(frame1,(14,14),cv2.BORDER_DEFAULT)
 
     hsv_threshold_hue = [12.949640287769784, 36.06060606060607]
-    hsv_threshold_saturation = [61.915467625899275, 207.77777777777777]
-    hsv_threshold_value = [82.55395683453237, 255.0]
+    hsv_threshold_saturation = [100.915467625899275, 207.77777777777777]
+    hsv_threshold_value = [150.55395683453237, 255.0]
     frame3 = __hsv_threshold(frame2, hsv_threshold_hue, hsv_threshold_saturation, hsv_threshold_value)
     #frame3 = cv2.inRange(frame2, (hsv_threshold_hue[0], hsv_threshold_saturation[0], hsv_threshold_value[0]),  (hsv_threshold_hue[1], hsv_threshold_saturation[1], hsv_threshold_value[1]))
 
     find_blobs_min_area = 0.0
-    find_blobs_circularity = [0.31654675658658255, 1.0]
+    find_blobs_circularity = [0.0, 1.0]
     find_blobs_dark_blobs = False
 
     keypoints = __find_blobs(frame3, find_blobs_min_area, find_blobs_circularity, find_blobs_dark_blobs)
