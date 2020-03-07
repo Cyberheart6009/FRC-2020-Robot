267ad452f51523e51c97943022bebb8b5d58c818
michael <michael.adragna1@gmail.com>
2020-03-07 10:01:32 -0500

    Track ball command and bot in auto accounts for offset

--- src/main/java/frc/robot/commands/BallTrackerCommand.java
+++ src/main/java/frc/robot/commands/BallTrackerCommand.java
@@ -8,6 +8,7 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ChassisSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,8 +20,15 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class BallTrackerCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final ChassisSubsystem m_chassisSubsystem;
-
-
+  private final IntakeSubsystem m_intakeSubsystem;
+  private int m_numBallsLeft;
+  private boolean m_ballCollected;
+  private double m_ballAngle;
+  private double m_ballDistance;
+  private boolean m_facingBall;
+  private double currentYaw;
+  private double newYaw;
+  private double startYaw;
 
 
   /**
@@ -28,8 +36,10 @@ public class BallTrackerCommand extends CommandBase {
    *
    * @param subsystem The subsystem used by this command.
    */
-  public BallTrackerCommand(ChassisSubsystem subsystem) {
+  public BallTrackerCommand(ChassisSubsystem subsystem, IntakeSubsystem intakeSubsystem, int numBallsLeft) {
     m_chassisSubsystem = subsystem;
+    m_intakeSubsystem = intakeSubsystem;
+    m_numBallsLeft = numBallsLeft;
 
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -40,32 +50,66 @@ public class BallTrackerCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_ballCollected = true;
+    m_facingBall = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    // get the ball's angle and distance relative to bot's current position
+    if (m_ballCollected == true) {
+      m_ballAngle = SmartDashboard.getNumber("BallAngle", 0.0);
+      m_ballDistance = SmartDashboard.getNumber("BallDistance", 0.0);
+      m_ballCollected = false;
+    }
 
+    // turn to ball
+    if (m_facingBall == false) {
+      if (m_chassisSubsystem.GetGyroAngle() > m_chassisSubsystem.GetGyroAngle() + m_ballAngle) {
+        m_chassisSubsystem.drive(0, -0.8);
+    } else if (m_chassisSubsystem.GetGyroAngle() < m_chassisSubsystem.GetGyroAngle() + m_ballAngle) {
+        m_chassisSubsystem.drive(0, 0.8);
+    } else {
+        m_facingBall = true;
+        startYaw = m_chassisSubsystem.getGyroYaw();
+      }
+    }
 
+    // Drive to ball
+    if (m_facingBall == true) {
+      if (m_chassisSubsystem.getDistance() < m_ballDistance + 20) {
+        currentYaw = m_chassisSubsystem.getGyroYaw();
+        newYaw = -(startYaw - currentYaw) / 12;
+        m_chassisSubsystem.drive(1, 0);
+        m_intakeSubsystem.setIntake(1);
+      } else {
+        m_numBallsLeft --;
+        m_ballCollected = true;
+        m_facingBall = false;
+      }
 
+    }        
   }
     
-
-  
 
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     System.out.println("ended");
-  }
+    m_intakeSubsystem.stopIntake();
 
+  }
 
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    if (m_numBallsLeft == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
